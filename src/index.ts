da9eb8b4565ad8fffa2ecb58c4c3e08b4afd4e81
Ramiz Midani <ramiz37@uab.edu>
2022-01-21 23:35:47 -0500

    forgot to fetch

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
 import { Client, Intents, Message } from 'discord.js';
 
-const dclient: Client<boolean> = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const dclient: Client<boolean> = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MEMBERS] });
 
 dclient.on('ready', () => {
     console.log(`Logged in as ${dclient.user?.tag}!`)
 });
 
 dclient.on('message', (msg: Message<boolean>) => {
+    msg.guild.members.fetch();
+    msg.guild.roles.fetch();
     const mod = '934241462533627905';
     let hasMod = msg.member.roles.cache.find(role => role.id == mod)
     if (!hasMod) return;
-    if (msg.channelId !== '934174624122535976') return;
-    if (!msg.content.startsWith('A New Wordle just started!')) return;
-    
+    // if (msg.channelId !== '934174624122535976') return;
+    // if (!msg.content.startsWith('A New Wordle just started!')) return;
+    if (!msg.content.startsWith('..')) return;
     const wordlePlayer = '934164576403087391';
     const wordleComplete = '934174868117794937';
-    const wordleCompleteRole = msg.guild.roles.cache.find(role => role.id == wordlePlayer);
+    const wordleCompleteRole = msg.guild.roles.cache.find(role => role.id == wordleComplete);
     msg.guild.members.cache.forEach(member => {
         console.log('loop: ', member.user.username)
         if (member.roles.cache.some(role => role.id == wordleComplete)) {
