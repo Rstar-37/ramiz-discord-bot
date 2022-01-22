a337703b419e858a370d4fd7d0ef0317cccc45e5
Ramiz Midani <ramiz37@uab.edu>
2022-01-21 20:29:41 -0500

    initial push

---
+++ src/index.ts
@@ -0,0 +1,30 @@
+import { Client, Intents, Message } from 'discord.js';
+
+const dclient: Client<boolean> = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+dclient.on('ready', () => {
+    console.log(`Logged in as ${dclient.user?.tag}!`)
+});
+
+dclient.on('message', (msg: Message<boolean>) => {
+    const mod = '934241462533627905';
+    let hasMod = msg.member.roles.cache.find(role => role.id == mod)
+    if (!hasMod) return;
+    if (msg.content !== 'remove wordle complete') return;
+    const wordlePlayer = '934164576403087391';
+    const wordleComplete = '934174868117794937';
+    msg.guild.members.cache.forEach(member => {
+        if (member.roles.cache.some(role => role.id == wordleComplete)) {
+            member.roles.remove(wordleComplete);
+        }
+    });
+    (msg.channel as any).send('Success!');
+});
+
+let discordToken = process.env.DISCORD_TOKEN;
+if (!discordToken) {
+    let auth = require('./bot.json');
+    discordToken = auth.token;
+}
+dclient.login(discordToken);
+
